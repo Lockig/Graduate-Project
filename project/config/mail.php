1e9fa00ee2b6e2c961948041f25714f4aed99f99
Lockig <minhntt2001hn@gmail.com>
2022-08-26 16:00:20 +0700

    mail

--- project/config/mail.php
+++ project/config/mail.php
@@ -92,7 +92,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'minhntt2001hn@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
