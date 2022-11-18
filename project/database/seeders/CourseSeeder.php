79ec7ce88593da9867a6b5f6f65156d19d22a414
Lockig <minhntt2001hn@gmail.com>
2022-11-18 19:26:08 +0700

    layout

--- project/database/seeders/CourseSeeder.php
+++ project/database/seeders/CourseSeeder.php
@@ -53,8 +53,17 @@ class CourseSeeder extends Seeder
         ]);
         DB::table('course_notifications')->insert([
             'course_id'=>'1',
-            'content'=>'chào mừng các em học sinh lớp 10'
+            'content'=>'chào mừng các em học sinh lớp 10',
+            'created_at'=>'2021-10-01 08:00:00'
         ]);
 
+        DB::table('attendances')->insert([
+            ['user_id'=>'2','schedule_id'=>'1','time_in'=>'2021-11-01 08:00:00'],
+            ['user_id'=>'4','schedule_id'=>'1','time_in'=>'2021-11-01 08:05:00'],
+            ['user_id'=>'5','schedule_id'=>'1','time_in'=>'2021-11-01 08:10:00'],
+            ['user_id'=>'6','schedule_id'=>'1','time_in'=>'2021-11-01 08:15:00'],
+            ['user_id'=>'7','schedule_id'=>'1','time_in'=>'2021-11-01 08:20:00'],
+            ['user_id'=>'8','schedule_id'=>'1','time_in'=>'2021-11-01 08:00:00'],
+        ]);
     }
 }
